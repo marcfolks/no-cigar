a617d8041121e7900fe7bfb0f13ffe586e809552
marcfolks <marcfolks@gmail.com>
2017-07-15 15:13:17 -0400

    lotto

--- lottotest.rb
+++ lottotest.rb
@@ -4,12 +4,13 @@ class Testlotto < Minitest::Test
 
 
 def test_winning_number_match
-	my_num = (["3264"])
-	win_num = ["3334","3345","3363","3247","3266"]
+	my_num =(["3264"])
+	win_num1 = ["3334","3345","3264","3247","3266"]
 	assert_equal(["3264"],my_num)
 end
- # def test_close_match
- # 	my_num = (["3264"])
-	# win_num = ["3334","3345","3363","3247","3266"]
- # 	assert_equal(["3204"],my_num)
+  def test_close_match
+  	my_num =(["4365"])
+	 win_num1 = ["3264","334565","3363","3247","3266"]
+  	assert_equal(["3456"],almost_there(my_num, win_num1))
+end
 end
--- winner.rb
+++ winner.rb
@@ -1,12 +1,42 @@
-def count_matching_chars(str1,str2)
-  str1.each_char.count do |char| 
-    str2.include?(char)
+def lottery(my_num, win_num1)
+  match = []
+  win_num1.each do |match_num|
+    if match_num == my_num
+      match << my_num
+    end
   end
+  match
 end
 
-def matches(my_num, arr)
-  arr.select do |num|
-    return true if count_matching_chars(num,my_num) >= 3
+def off_by_one(my_num, win_num1)
+  count = 0
+  z=0
+   win_num1.length.times do
+     i=0
+    win_num1.length.times do
+      if my_num[z] == win_num1[i]
+        count += 1
+        # p count
+      end
+      # puts i
+      i+=1
   end
+  z+=1
 end
+  count == win_num1.length - 1
+end
+p off_by_one("28760275937234",a=["287602759387234"])
+
+def almost_there(my_num, win_num1)
+  winning = []
+  win_num1.each do |y|
+    if off_by_one(my_num, y) == true
+      winning << y
+    end
+  end
+  winning
+end
+
+
+
 
